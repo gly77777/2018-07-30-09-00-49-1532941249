905ee759caa7b3a092fc7fde05735281f3c4afcb
guliuyi <glyi7@qq.com>
2018-08-01 17:43:19 +0800

    no.2

--- main/main.js
+++ main/main.js
@@ -1,18 +1,44 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+    console.log(`o) 最小值 = ${sequence.minimum()}`,
+                `o) 最大值 = ${sequence.max()}`,
+                `o) 平均值 = ${sequence.average()}`,
+                `o) 元素数量 = ${sequence.length}`);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+    this.sequence=sequence;
   }
 
   minimum() {
-    // Write your code here
+    var min=this[0];
+    var len=this.length;
+    for(i=1;i<len;i++){
+       if(this[i]<min){
+         min=this[i];
+       } 
+    }
+    return min;
   }
 
-  // Write your code here
+  max() {
+    var max=this[0];
+    var len=this.length;
+    for(i=1;i<len;i++){
+       if(this[i]>max){
+         max=this[i];
+       } 
+    }
+    return max;
+  }
+
+  average() {
+    var sum=this[0];
+    var len=this.length;
+    for(i=1;i<len;i++){
+        sum+=this[i];
+    } 
+    return sum/len;
+  }
 }
